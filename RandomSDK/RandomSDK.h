f9eaf6bcb9a8f49c67fc14f2dc38e464b5ec7455
pavanidivya48 <pavanidivya48@gmail.com>
2019-08-06 18:18:27 +0530

    Initial Commit

---
+++ RandomSDK/RandomSDK.h
@@ -0,0 +1,19 @@
+//
+//  RandomSDK.h
+//  RandomSDK
+//
+//  Created by Ats on 06/08/19.
+//  Copyright © 2019 Ats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RandomSDK.
+FOUNDATION_EXPORT double RandomSDKVersionNumber;
+
+//! Project version string for RandomSDK.
+FOUNDATION_EXPORT const unsigned char RandomSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomSDK/PublicHeader.h>
+
+
