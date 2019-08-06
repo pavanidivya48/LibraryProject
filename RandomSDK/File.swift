2f0108eee1f9d7a004317ba444fab9c81dffa80d
pavanidivya48 <pavanidivya48@gmail.com>
2019-08-06 18:21:19 +0530

    testing

---
+++ RandomSDK/File.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  RandomSDK
+//
+//  Created by Ats on 06/08/19.
+//  Copyright © 2019 Ats. All rights reserved.
+//
+
+import Foundation
+
+public class RandomGenerator {
+    private init() { }
+    
+    public static func string() -> String {
+        return UUID().uuidString
+    }
+    
+    public static func integer() -> Int {
+        return Int(arc4random()) // something to do with this
+    }
+    
+}
